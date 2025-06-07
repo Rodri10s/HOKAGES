0137f498d8227a9bc055928ee2add0efcb43eb92
Ramon Rodrigues <ramonrodrigues838@gmail.com>
2025-06-07 00:07:57 -0300

    alteração das imagens de inicio

--- plugins/script.js
+++ plugins/script.js
@@ -1,8 +1,9 @@
 $(document).ready(function () {
-    $("#banners ul").bxSlider({
-        auto: true,
-        speed: 1000,
-        // mode: 'fade'
-        // pager: false
-    });
+     $(".inicio .background-section #banners ul").bxSlider({
+         auto: true, // inicia o slider automaticamente
+         speed: 2000, // velocidade da transição
+         pause: 5000, // tempo de pausa entre as transições
+         mode: 'fade', // modo de transição
+         infiniteLoop: true // para o slide ao chegar na última imagem
+     });
 });
